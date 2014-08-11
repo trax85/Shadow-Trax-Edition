8abf701f53468d70df20c98e6c20431f4e9074e1
Will Deacon <will.deacon@arm.com>
2014-08-11 14:23:37 +0100

    arm64: compat: wire up memfd_create and getrandom syscalls for aarch32
    arch/arm/ just grew support for the new memfd_create and getrandom
    syscalls, so add them to our compat layer too.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/include/asm/unistd32.h
+++ arch/arm64/include/asm/unistd32.h
@@ -781,13 +781,16 @@ __SYSCALL(__NR_process_vm_writev, compat_sys_process_vm_writev)
 __SYSCALL(__NR_kcmp, sys_kcmp)
 #define __NR_finit_module 379
 __SYSCALL(__NR_finit_module, sys_finit_module)
-#define __NR_sys_sched_setattr 380
-__SYSCALL(__NR_sys_sched_setattr, sys_sched_setattr)
-#define __NR_sys_sched_getattr 381
-__SYSCALL(__NR_sys_sched_getattr, sys_sched_getattr)
-/* #define __NR_renameat2 382 */
-__SYSCALL(382, sys_ni_syscall)
+#define __NR_sched_setattr 380
+__SYSCALL(__NR_sched_setattr, sys_sched_setattr)
+#define __NR_sched_getattr 381
+__SYSCALL(__NR_sched_getattr, sys_sched_getattr)
+/* Backporting seccomp, stub out missing renameat2 call */
+#define __NR_renameat2 382
+__SYSCALL(__NR_renameat2, sys_ni_syscall)
 #define __NR_seccomp 383
 __SYSCALL(__NR_seccomp, sys_seccomp)
 #define __NR_getrandom 384
 __SYSCALL(__NR_getrandom, sys_getrandom)
+#define __NR_memfd_create 385
+__SYSCALL(__NR_memfd_create, sys_memfd_create)
