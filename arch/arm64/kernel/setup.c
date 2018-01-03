dd159326c54a8d98f5d96084e7c9ef6461de248a
Will Deacon <will.deacon@arm.com>
2018-01-03 11:17:58 +0000

    arm64: Add skeleton to harden the branch predictor against aliasing a… …ttacks.
    Aliasing attacks against CPU branch predictors can allow an attacker to
    redirect speculative control flow on some CPUs and potentially divulge
    information from one context to another.
    
    This patch adds initial skeleton code behind a new Kconfig option to
    enable implementation-specific mitigations against these attacks for
    CPUs that are affected.
    
    Change-Id: Idf832a78acc728a8b535e904482d0e14dd2db2bf
    Co-developed-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Git-commit: 7bd293b6845d003ab087faa6515a626c8703b8da
    Git-repo: https://git.kernel.org/pub/scm/linux/kernel/git/arm64/linux
    [neeraju@codeaurora.org: resolve merge conflicts and context
     conflicts. Ignore changes in missing files: cpucaps.h,
     sysreg.h, cpu_errata.c, cpufeature.c. Remove KVM, hyp
     specific changes. Port bp hardening installation from
     missing cpu_errata.c to setup.c, ignoring the missing
     features like capability matches, feature register
     checks and KVM specific code. Fix compilation due to
     missing includes in mmu.h.]
    Signed-off-by: Neeraj Upadhyay <neeraju@codeaurora.org>
    Signed-off-by: Srinivasarao P <spathi@codeaurora.org>
    Signed-off-by: Rajshekar Eashwarappa <reashw@codeaurora.org>

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -211,7 +211,23 @@ static void __init smp_build_mpidr_hash(void)
 }
 #endif
 
-static void __init setup_processor(void)
+#ifdef CONFIG_HARDEN_BRANCH_PREDICTOR
+#include <asm/mmu_context.h>
+
+DEFINE_PER_CPU_READ_MOSTLY(struct bp_hardening_data, bp_hardening_data);
+
+static void __maybe_unused __install_bp_hardening_cb(bp_hardening_cb_t fn)
+{
+	__this_cpu_write(bp_hardening_data.fn, fn);
+}
+
+static void __maybe_unused install_bp_hardening_cb(bp_hardening_cb_t fn)
+{
+	__install_bp_hardening_cb(fn);
+}
+#endif	/* CONFIG_HARDEN_BRANCH_PREDICTOR */
+
+void __init setup_cpu_features(void)
 {
 	struct cpu_info *cpu_info;
 	u64 features, block;
@@ -307,6 +323,25 @@ static void __init setup_processor(void)
 	if (block && !(block & 0x8))
 		compat_elf_hwcap2 |= COMPAT_HWCAP2_CRC32;
 #endif
+}
+
+static void __init setup_processor(void)
+{
+	struct cpu_info *cpu_info;
+
+	cpu_info = lookup_processor_type(read_cpuid_id());
+	if (!cpu_info) {
+		printk("CPU configuration botched (ID %08x), unable to continue.\n",
+		       read_cpuid_id());
+		while (1);
+	}
+
+	cpu_name = cpu_info->cpu_name;
+
+	printk("CPU: %s [%08x] revision %d\n",
+	       cpu_name, read_cpuid_id(), read_cpuid_id() & 15);
+
+	sprintf(init_utsname()->machine, ELF_PLATFORM);
 }
 
 static void __init setup_machine_fdt(phys_addr_t dt_phys)
