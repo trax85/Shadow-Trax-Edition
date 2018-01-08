bcf3961443656160123248265c0c7c32f7ddfe2c
Daniel Rosenberg <drosen@google.com>
2018-01-08 13:57:36 -0800

    ANDROID: sdcardfs: Fix missing break on default_normal
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Bug: 64672411
    Change-Id: I98796df95dc9846adb77a11f49a1a254fb1618b1
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -122,12 +122,12 @@ static int parse_options(struct super_block *sb, char *options, int silent,
 				return 0;
 			opts->reserved_mb = option;
 			break;
-                case Opt_gid_derivation:
- 			opts->gid_derivation = true;
- 			break;
-                case Opt_default_normal:
- 			opts->default_normal = true;
- 			break;
+		case Opt_gid_derivation:
+			opts->gid_derivation = true;
+			break;
+		case Opt_default_normal:
+			vfsopts->default_normal = true;
+			break;
 		/* unknown option */
 		default:
 			if (!silent)
