d424ade9dbbc83ef58f4bf81b64f8c64101c1e8c
tejas <tejasudupa1285@gmail.com>
2020-01-01 12:15:28 +0530

    arm64:kernel/arm64ksyms.c:Export symbols for strcmp and strncmp

--- arch/arm64/kernel/arm64ksyms.c
+++ arch/arm64/kernel/arm64ksyms.c
@@ -49,6 +49,8 @@ EXPORT_SYMBOL(strcmp);
 EXPORT_SYMBOL(strncmp);
 EXPORT_SYMBOL(strlen);
 EXPORT_SYMBOL(strnlen);
+EXPORT_SYMBOL(strcmp);
+EXPORT_SYMBOL(strncmp);
 EXPORT_SYMBOL(memset);
 EXPORT_SYMBOL(memcpy);
 EXPORT_SYMBOL(memmove);
