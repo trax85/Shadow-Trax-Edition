6cc9448f6f02a86db257807638affd55ca2abba8
tejas <tejasudupa1285@gmail.com>
2020-03-25 05:16:38 +0530

    sched/tune:Add support for Dynamic SchedTune Boost and add missing functions

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -10,11 +10,25 @@
 #include <trace/events/sched.h>
 
 #include "sched.h"
+#include "tune.h"
 
 unsigned int sysctl_sched_cfs_boost __read_mostly = 0;
 
 
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+#define DYNAMIC_BOOST_SLOTS_COUNT 5
+static DEFINE_MUTEX(boost_slot_mutex);
+static DEFINE_MUTEX(stune_boost_mutex);
+static struct schedtune *getSchedtune(char *st_name);
+static int dynamic_boost(int boost);
 
+struct boost_slot {
+ 	struct list_head list;
+ 	int idx;
+};
+static struct schedtune *st_ta;
+
+#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 /* Performance Boost region (B) threshold params */
 static int perf_boost_idx;
