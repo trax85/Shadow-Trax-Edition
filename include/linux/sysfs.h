6b20deab3e469ba61e1dc0cbbaa6e89cbe96aff2
tejas <tejasudupa1285@gmail.com>
2020-04-12 01:06:43 +0530

    Revert:sysfs.h:add __ATT_RW macro for schedutil missing fucntion

--- include/linux/sysfs.h
+++ include/linux/sysfs.h
@@ -79,9 +79,6 @@ struct attribute_group {
 	.show	= _name##_show,					\
 }
 
-#define __ATTR_RW(_name) __ATTR(_name, (S_IWUSR | S_IRUGO),		\
-			 _name##_show, _name##_store)
-
 #define __ATTR_NULL { .attr = { .name = NULL } }
 
 #ifdef CONFIG_DEBUG_LOCK_ALLOC
