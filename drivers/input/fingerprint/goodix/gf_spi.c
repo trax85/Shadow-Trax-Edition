0c82d31038527c46539e8880fefa48b16110aa7e
tejas <tejasudupa1285@gmail.com>
2020-04-20 23:38:57 +0530

    Revert:Make irq input fingerprint Perf ctrical
    Some Users are facing black screen in lockscreen and reboots so revert

--- drivers/input/fingerprint/goodix/gf_spi.c
+++ drivers/input/fingerprint/goodix/gf_spi.c
@@ -569,7 +569,7 @@ static int driver_init_partial(struct gf_dev *gf_dev)
 	gf_dev->irq = gf_irq_num(gf_dev);
 #if 1
 	ret = request_threaded_irq(gf_dev->irq, NULL, gf_irq,
-			IRQF_TRIGGER_RISING | IRQF_ONESHOT | IRQF_PERF_CRITICAL,
+			IRQF_TRIGGER_RISING | IRQF_ONESHOT,
 			"gf", gf_dev);
 #else
 	ret = request_irq(gf_dev->irq, gf_irq,
