57d015f2863e277bc4460bbb6f949c88a59a5408
tejas <tejasudupa1285@gmail.com>
2020-05-05 00:28:23 +0530

    cpufreq.c:Add back Missing voltage control for kenzo(GPU and CPU)

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -31,6 +31,21 @@
 #include <linux/pm_opp.h>
 #include <trace/events/power.h>
 
+
+extern ssize_t gpu_clock_get_vdd(char *buf);
+extern ssize_t gpu_clock_set_vdd(const char *buf, size_t count);
+
+static ssize_t show_GPU_mV_table(struct cpufreq_policy *policy, char *buf)
+{
+	return gpu_clock_get_vdd(buf);
+}
+
+static ssize_t store_GPU_mV_table(struct cpufreq_policy *policy,
+	const char *buf, size_t count)
+{
+	return gpu_clock_set_vdd(buf, count);
+}
+
 /**
  * The "cpufreq driver" - the arch- or hardware-dependent low
  * level driver of CPUFreq support, and its spinlock. This lock
@@ -850,7 +865,7 @@ static ssize_t show_bios_limit(struct cpufreq_policy *policy, char *buf)
 	return sprintf(buf, "%u\n", policy->cpuinfo.max_freq);
 }
 
-#ifdef CONFIG_VOLTAGE_CONTROL
+#ifdef CONFIG_MACH_XIAOMI_KENZO
 extern ssize_t cpu_clock_get_vdd(char *buf);
 extern ssize_t cpu_clock_set_vdd(const char *buf, size_t count);
 
@@ -880,7 +895,8 @@ cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
-#ifdef CONFIG_VOLTAGE_CONTROL
+cpufreq_freq_attr_rw(GPU_mV_table);
+#ifdef CONFIG_MACH_XIAOMI_KENZO
 cpufreq_freq_attr_rw(UV_mV_table);
 #endif
 
@@ -896,7 +912,8 @@ static struct attribute *default_attrs[] = {
 	&scaling_driver.attr,
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
-#ifdef CONFIG_VOLTAGE_CONTROL
+	&GPU_mV_table.attr,
+#ifdef CONFIG_MACH_XIAOMI_KENZO
 	&UV_mV_table.attr,
 #endif
 	NULL
