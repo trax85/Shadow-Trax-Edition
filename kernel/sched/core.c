32d605f358d25a34231992400d52dad483730674
tejas <tejasudupa1285@gmail.com>
2020-05-15 04:34:16 +0530

    kthread: Add API to create kthreads bound to low-power CPUs Some unimportant kthreads do not need high-performance and can safely be bound to low-power CPUs for energy efficiency.
    This commit adds an API to create kthreads bound to low-power CPUs. This
    affinity cannot be altered by the kernel or userspace.
    
    This extends the performance-critical kthread support introduced in
    commit c4a0ef7e1d1e ("kernel: Add API to mark IRQs and kthreads as
    performance critical").
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1239,6 +1239,9 @@ get_adjusted_cpumask(const struct task_struct *p,
 	/* Force all low-power kthreads onto the big cluster */
 	if (p->flags & PF_PERF_CRITICAL)
 		return cpu_lp_mask;
+	/* Force all low-power kthreads onto the little cluster */
+ 	if (p->flags & PF_LOW_POWER)
+ 		return cpu_lp_mask;
 
 	return orig_mask;
 }
