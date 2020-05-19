9cd646e0022db66b20e60b0e53a7df94608ae732
tejas <tejasudupa1285@gmail.com>
2020-05-20 00:54:26 +0530

    Revert back to our old Cpufreq setup

--- drivers/clk/qcom/clock-cpu-8976.c
+++ drivers/clk/qcom/clock-cpu-8976.c
@@ -186,7 +186,7 @@ static struct pll_clk a53ss_sr_pll = {
 		.config_ctl_val = 0x00141400,
 	},
 	.min_rate = 652800000,
-	.max_rate = 1440000000,
+	.max_rate = 1401600000,
 	.base = &virt_bases[APCS_C0_PLL_BASE],
 	.c =  {
 		.parent = &xo_a_clk.c,
