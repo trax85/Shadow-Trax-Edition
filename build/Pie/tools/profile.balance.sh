6d1b353cf7a26dc59c69367e839c6de57df8d58a
tejas <tejasudupa1285@gmail.com>
2020-06-21 01:02:44 +0530

    Flash_tools:Tune up the performance in balance mode a bit

--- build/Pie/tools/profile.balance.sh
+++ build/Pie/tools/profile.balance.sh
@@ -12,7 +12,7 @@ function write() {
 echo schedutil > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 400000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo 1401600 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-echo 960 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us
+echo 950 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us
 echo 1000 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us
 echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/io_wait_boost
 echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/exp_util
