95e744d92e8785eb5e74a97a88335fddf187b4cb
tejas <tejasudupa1285@gmail.com>
2020-07-10 00:58:40 +0530

    timer.c:Remove more hmp stuff

--- kernel/timer.c
+++ kernel/timer.c
@@ -386,23 +386,17 @@ __internal_add_timer(struct tvec_base *base, struct timer_list *timer)
 	list_add_tail(&timer->entry, vec);
 }
 
-static int internal_add_timer(struct tvec_base *base, struct timer_list *timer)
+static void internal_add_timer(struct tvec_base *base, struct timer_list *timer)
 {
-	int leftmost = 0;
-
 	__internal_add_timer(base, timer);
 	/*
 	 * Update base->active_timers and base->next_timer
 	 */
 	if (!tbase_get_deferrable(timer->base)) {
-		if (time_before(timer->expires, base->next_timer)) {
-			leftmost = 1;
+		if (time_before(timer->expires, base->next_timer))
 			base->next_timer = timer->expires;
-		}
 		base->active_timers++;
 	}
-
-	return leftmost;
 }
 
 #ifdef CONFIG_DEBUG_OBJECTS_TIMERS
@@ -712,7 +706,7 @@ __mod_timer(struct timer_list *timer, unsigned long expires,
 {
 	struct tvec_base *base, *new_base;
 	unsigned long flags;
-	int ret = 0, cpu, leftmost;
+	int ret = 0, cpu;
 
 	BUG_ON(!timer->function);
 
@@ -724,15 +718,15 @@ __mod_timer(struct timer_list *timer, unsigned long expires,
 
 	debug_activate(timer, expires);
 
-	cpu = smp_processor_id();
 
 #ifdef CONFIG_SMP
 	if (base != tvec_base_deferral) {
 #endif
 
+		cpu = smp_processor_id();
 #if defined(CONFIG_NO_HZ_COMMON) && defined(CONFIG_SMP)
-	if (!pinned && get_sysctl_timer_migration())
-		cpu = get_nohz_timer_target();
+		if (!pinned && get_sysctl_timer_migration())
+			cpu = get_nohz_timer_target();
 #endif
 		new_base = per_cpu(tvec_bases, cpu);
 
@@ -758,23 +752,7 @@ __mod_timer(struct timer_list *timer, unsigned long expires,
 #endif
 
 	timer->expires = expires;
-	leftmost = internal_add_timer(base, timer);
-
-#ifdef CONFIG_SCHED_HMP
-	/*
-	 * Check whether the other CPU is in dynticks mode and needs
-	 * to be triggered to reevaluate the timer wheel.
-	 * We are protected against the other CPU fiddling
-	 * with the timer by holding the timer base lock. This also
-	 * makes sure that a CPU on the way to stop its tick can not
-	 * evaluate the timer wheel.
-	 *
-	 * This test is needed for only CONFIG_SCHED_HMP, as !CONFIG_SCHED_HMP
-	 * selects non-idle cpu as target of timer migration.
-	 */
-	if (cpu != smp_processor_id() && leftmost)
-		wake_up_nohz_cpu(cpu);
-#endif
+	internal_add_timer(base, timer);
 
 out_unlock:
 	spin_unlock_irqrestore(&base->lock, flags);
@@ -929,29 +907,14 @@ EXPORT_SYMBOL(add_timer);
  */
 void add_timer_on(struct timer_list *timer, int cpu)
 {
-	struct tvec_base *new_base = per_cpu(tvec_bases, cpu);
-	struct tvec_base *base;
+	struct tvec_base *base = per_cpu(tvec_bases, cpu);
 	unsigned long flags;
-	int leftmost;
 
 	BUG_ON(timer_pending(timer) || !timer->function);
-
-	/*
-	 * If @timer was on a different CPU, it should be migrated with the
-	 * old base locked to prevent other operations proceeding with the
-	 * wrong base locked.  See lock_timer_base().
-	 */
-	base = lock_timer_base(timer, &flags);
-	if (base != new_base) {
-		timer_set_base(timer, NULL);
-		spin_unlock(&base->lock);
-		base = new_base;
-		spin_lock(&base->lock);
-		timer_set_base(timer, base);
-	}
+	spin_lock_irqsave(&base->lock, flags);
+	timer_set_base(timer, base);
 	debug_activate(timer, timer->expires);
-	leftmost = internal_add_timer(base, timer);
-
+	internal_add_timer(base, timer);
 	/*
 	 * Check whether the other CPU is in dynticks mode and needs
 	 * to be triggered to reevaluate the timer wheel.
@@ -960,8 +923,7 @@ void add_timer_on(struct timer_list *timer, int cpu)
 	 * makes sure that a CPU on the way to stop its tick can not
 	 * evaluate the timer wheel.
 	 */
-	if (leftmost)
-		wake_up_nohz_cpu(cpu);
+	wake_up_nohz_cpu(cpu);
 	spin_unlock_irqrestore(&base->lock, flags);
 }
 EXPORT_SYMBOL_GPL(add_timer_on);
