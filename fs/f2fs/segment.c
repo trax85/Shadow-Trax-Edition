psndna88/AGNi_pureMIUI

Convert many kenzo & qcom drivers to use msecs_to_jiffies insted of HZ
Needed for easy switching to 300HZ+ kernel. some drivers need 100HZ

Signed-off-by: Parvinder Singh <psndna88@gmail.com>

fs/f2fs/segment.c
@@ -294,7 +294,7 @@ retry:
 			err = get_dnode_of_data(&dn, page->index, LOOKUP_NODE);
 			if (err) {
 				if (err == -ENOMEM) {
-					congestion_wait(BLK_RW_ASYNC, HZ/50);
+					congestion_wait(BLK_RW_ASYNC, msecs_to_jiffies(20));
 					cond_resched();
 					goto retry;
 				}
@@ -401,7 +401,7 @@ retry:
 			err = do_write_data_page(&fio);
 			if (err) {
 				if (err == -ENOMEM) {
-					congestion_wait(BLK_RW_ASYNC, HZ/50);
+					congestion_wait(BLK_RW_ASYNC, msecs_to_jiffies(20));
 					cond_resched();
 					goto retry;
 				}

trax85/Shadow-Trax-Edition

fs/f2fs/segment.c
@@ -895,7 +895,7 @@ repeat:
 	blk_finish_plug(&plug);
 
 	iter = 0;
-	congestion_wait(BLK_RW_SYNC, HZ/50);
+	congestion_wait(BLK_RW_SYNC, msecs_to_jiffies(20));
 
 	wait_event_interruptible(*q,
 		kthread_should_stop() || !list_empty(&dcc->discard_cmd_list));