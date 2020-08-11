5c88d0b178367e977046e0b178176ff266488d72
tejas <tejasudupa1285@gmail.com>
2020-08-11 05:48:24 +0530

    Revert a few things for cleanliness

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -40,9 +40,6 @@
 
 #define XO_CLK_RATE	19200000
 
-static unsigned int dsi_refreshrate = 60;
-module_param(dsi_refreshrate, uint, 0755);
-
 /* Master structure to hold all the information about the DSI/panel */
 static struct mdss_dsi_data *mdss_dsi_res;
 
@@ -1863,7 +1860,6 @@ static int mdss_dsi_dfps_config(struct mdss_panel_data *pdata, int new_fps)
 	if (sctrl_pdata)
 		sctrl_pdata->dfps_status = true;
 
-        new_fps = dsi_refreshrate;
 	if (new_fps !=
 		ctrl_pdata->panel_data.panel_info.mipi.frame_rate) {
 		if (pdata->panel_info.dfps_update
