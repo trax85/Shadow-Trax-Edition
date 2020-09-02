e33df749e9e2dc36ba11826af749c37be536f4d9
tejas <tejasudupa1285@gmail.com>
2020-09-02 16:49:45 +0530

    wcd9xxx-core:Fix warning fortify-source clang warns: drivers/mfd/wcd9xxx-core.c:1735:9: warning: 'snprintf' size argument is too large; destination buffer has size 20, but size argument is 25 [-Wfortify-source]                 len = snprintf(tmp_buf, 25, "0x%.3x: 0x%.2x\n", i, reg_val);

--- drivers/mfd/wcd9xxx-core.c
+++ drivers/mfd/wcd9xxx-core.c
@@ -1732,7 +1732,7 @@ static ssize_t wcd9xxx_slimslave_reg_show(char __user *ubuf, size_t count,
 
 	for (i = (int) *ppos / 12; i <= SLIM_MAX_REG_ADDR; i++) {
 		reg_val = wcd9xxx_interface_reg_read(debugCodec, i);
-		len = snprintf(tmp_buf, 25, "0x%.3x: 0x%.2x\n", i, reg_val);
+		len = snprintf(tmp_buf, sizeof(tmp_buf), "0x%.3x: 0x%.2x\n", i, reg_val);
 
 		if ((total + len) >= count - 1)
 			break;
